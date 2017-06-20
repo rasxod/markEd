9b2e2939a4cf413af6690cc225835261f3be70c3
Nezabor <st@ssmart.ru>
2017-06-20 08:43:58 +0300

    Update parsedown.php

--- class/parsedown.php
+++ class/parsedown.php
@@ -1,15 +1,6 @@
 <?php
 
-#
-#
-# Parsedown
-# http://parsedown.org
-#
-# (c) Emanuil Rusev
-# http://erusev.com
-#
-# For the full license information, view the LICENSE file that was distributed
-# with this source code.
+
 #
 #
 
